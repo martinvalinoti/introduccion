f07e58bd2f66846bd9476d6336d86e66778ef659
martinvalinoti <martinvalinoti@gmail.com>
2023-01-20 13:24:06 -0300

    Main

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .views import HomeView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name="home")
 ]
